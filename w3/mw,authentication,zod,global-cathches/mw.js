fd974d72e270827845a985ea42f560c3738c9baa
Akshay-Varma7 <jampaniakshay@gmail.com>
2025-09-12 20:39:13 +0530

    error

--- w3/mw,authentication,zod,global-cathches/mw.js
+++ w3/mw,authentication,zod,global-cathches/mw.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-
+//one req & res(objects) passed through all mw in order, each uses/modifies what it needs, and only next() moves to the next
 function userMw(req,res,next){//3 params-for Mw
+    const {username , password} =req.query;//memory for this stack
     if(username!="akshay"|| password!="1234"){
         res.status(403).json({//403 - refuses to authorize it
             msg:"Invalid username or password!"
@@ -13,6 +14,7 @@ function userMw(req,res,next){//3 params-for Mw
     }
 }
 function kidneyMw(req,res,next){
+    const {kidney} = req.query;//memory for this stack
     if(kidney!=1 && kidney!=2){//if || then even for 2- true
         res.status(403).json({
             msg:"Invalid kidneys!"
@@ -22,10 +24,12 @@ function kidneyMw(req,res,next){
     }
 }
 //here if of MW satisfy then the handler using this MW wont progress as no next
-app.get("/health-checkup",userMw,kidneyMw,function (req,res){
+app.get("/health-checkup",userMw,kidneyMw,function (req,res){//order of mw
     res.send("your healthy");
 })
-app.get("/heart-checkup",userMw,function (req,res){//here no need kidney check
+app.get("/heart-checkup",userMw,function (req,res){//acc to context -mw
     res.send("your heart is healthy");
 })
-app.listen(3000);
+app.listen(3000,()=>{
+    console.log("server is running on port 3000");
+})
